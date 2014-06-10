482d9a28ea3e2314c5f0360339224bb2d787a538
Florian Pilz <fp@gocept.com>
2014-06-10 11:03:02 +0200

    Add JS library to mock ajax calls during jasmine tests.

--- src/gocept/jasmine/resource.py
+++ src/gocept/jasmine/resource.py
@@ -11,11 +11,13 @@ library = fanstatic.Library('gocept.jasmine', 'resources', minifiers=MINIFIERS)
 
 jasmine_css = fanstatic.Resource(library, 'jasmine.css')
 jasmine_js = fanstatic.Resource(library, 'jasmine.js')
+mock_ajax_js = fanstatic.Resource(
+    library, 'mock-ajax.js', depends=[jasmine_js])
 jasmine_html = fanstatic.Resource(
     library, 'jasmine-html.js', depends=[jasmine_js])
 
 jasmine = fanstatic.Resource(
     library,
     'setup_jasmine.js',
-    depends=[jasmine_css, jasmine_html],
+    depends=[jasmine_css, jasmine_html, mock_ajax_js],
     bottom=False)
