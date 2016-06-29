563c75a74211a591e6dd082916e1f2fbc784dd8e
Steffen Allner <sa@gocept.com>
2016-06-29 16:14:54 +0200

    (c), and URL fixes.

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-# Copyright (c) 2013 gocept gmbh & co. kg
-# See also LICENSE.txt
-
 # This should be only one line. If it must be multi-line, indent the second
 # line onwards to keep the PKG-INFO file format intact.
 """Jasmine integration for selenium.
